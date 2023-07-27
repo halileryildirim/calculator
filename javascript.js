1f4978926f927b1024a00ccbabd069d475e93775
halileryildirim <halileryildirim@gmail.com>
2023-07-27 20:07:03 +0300

    Updated operator functions with simpler code

--- javascript.js
+++ javascript.js
@@ -1,24 +1,21 @@
 let value3;
 
 const add = function(value1, value2) {
-    value3 = value1 + value2;
-    return value3;
+    return value1 + value2;
 }
 const subtract = function(value1, value2) {
-    value3 = value1 - value2;
-    return value3;
+    return value1 - value2;
 }
 
 const multiply = function(value1, value2) {
-    value3 = value1 * value2;
-    return value3;
+    return value1 * value2;
 }
 
 const divide = function(value1, value2) {
-    value3 = value1 / value2;
-    return value3;
+    return value1 / value2;
 }
 
 const operate = function(operator, value1, value2) {
     return operator(value1,value2);
 }
+

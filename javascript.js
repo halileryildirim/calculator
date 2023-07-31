90de19d0f93f2a96e0a552fa6830703f9e1f0f45
halileryildirim <halileryildirim@gmail.com>
2023-07-31 17:24:35 +0300

    Added decimal function

--- javascript.js
+++ javascript.js
@@ -14,32 +14,24 @@ const multiply = function(value1, value2) {
     return value1 * value2;
 };
 const divide = function(value1, value2) {
-    if(value2 == 0) {
-        currentValue = '';
-        previousValue = '';
-        currValue.innerText = "ERROR";
-    }
-    else {
-        return value1 / value2;
-    }
-    
+    return value1 / value2;
 };
 
 const operate = function(operator, value1, value2) {
     if(operator == "+") {
-        console.log("+ geldi")
         return add(value1, value2);
     }
     else if(operator == "-") {
-        console.log("- geldi")
         return subtract(value1, value2);
     }
     else if(operator == "x") {
-        console.log("x geldi")
         return multiply(value1, value2);
     }
     else if(operator == "/") {
-        console.log("/ geldi")
+        if(currentValue == 0) {
+            currentValue = "ERROR";
+            currValue.innerText = currentValue;
+        }
         return divide(value1, value2);
     }
 };
@@ -50,6 +42,7 @@ const subtractButton = document.querySelector(".subtract");
 const divideButton = document.querySelector(".divide");
 const multiplyButton = document.querySelector(".multiply");
 const clearButton = document.querySelector(".clear");
+const decimalButton = document.querySelector(".decimal");
 const equalsButton = document.querySelector(".equals");
 const preValue = document.querySelector(".previous");
 const currValue = document.querySelector(".current");
@@ -60,11 +53,23 @@ numberButtons.forEach(button => {
             currentValue += button.innerText; 
             currValue.innerText = currentValue;
         }
+        else if(currentValue == "ERROR") {
+            currentValue = "";
+            currValue.innerText = currentValue;
+        }
         else {
             alert("ERROR");
         }   
         
     })
+})
+
+decimalButton.addEventListener('click', () => {
+    if(!currentValue.includes(".")) {
+        currentValue += ".";
+        currValue.innerText = currentValue;
+
+    }
 })
 
 clearButton.addEventListener('click', () => {
@@ -82,7 +87,6 @@ addButton.addEventListener('click', () => {
     preValue.innerText = previousValue;
     currentValue = '';
     currValue.innerText = currentValue;
-    console.log(operator);
 });
 
 subtractButton.addEventListener('click', () => {
@@ -91,7 +95,6 @@ subtractButton.addEventListener('click', () => {
     preValue.innerText = previousValue;
     currentValue = '';
     currValue.innerText = currentValue;
-    console.log(operator);
 });
 
 multiplyButton.addEventListener('click', () => {
@@ -100,7 +103,6 @@ multiplyButton.addEventListener('click', () => {
     preValue.innerText = previousValue;
     currentValue = '';
     currValue.innerText = currentValue;
-    console.log(operator);
 })
 
 divideButton.addEventListener('click', () => {
@@ -109,36 +111,47 @@ divideButton.addEventListener('click', () => {
     preValue.innerText = previousValue;
     currentValue = '';
     currValue.innerText = currentValue;
-    console.log(operator);
 });
+
 
 
 equalsButton.addEventListener('click', () => {
     if(previousValue == "" && currentValue == "") {
         currValue.innerText = "ERROR";
     }
-    else if(currentValue != "" && previousValue == "") {
+    else if(previousValue != "" && currentValue == "") {
         currValue.innerText = currentValue;
     }
     else {
-        previousValue = parseInt(previousValue);
-        currentValue = parseInt(currentValue);
+        previousValue = parseFloat(previousValue);
+        currentValue = parseFloat(currentValue);
         currentValue = operate(operator, previousValue, currentValue);
-        previousValue ="";
         preValue.innerText = previousValue;
         currValue.innerText = currentValue;
+
+        if(currentValue == "Infinity") {
+            currentValue = "";
+            previousValue = "";
+            currValue.innerText = "ERROR";
+            preValue.innerText = previousValue;
+        };
     }
 
 });
 
 
+window.addEventListener('click', () => {
+    console.log("crnt: " + currentValue);
+    console.log("prvs: " + previousValue);
+})
 
 /*
 
-Divide by zero not working
-Numbers are flowing out of display
-Decimal not working
+Divide by zero error not working -solved
+Equal after operator is causing errors. -solved
+Decimal not working -solved
 Floats are huge
+Numbers are flowing out of display
 innertext everytime is not a good practice -> build a function
 
 */
